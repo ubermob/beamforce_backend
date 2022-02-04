8d0483a0d855a8eac483a0d4195476719b885a48
uber <savg@mail.ru>
2022-02-04 19:43:24 +0300

    First commit

---
+++ src/main/java/ru/beamforce/entity/User.java
@@ -0,0 +1,57 @@
+package ru.beamforce.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+/**
+ * @author Andrey Korneychuk on 04-Feb-22
+ * @version 1.0
+ */
+@Entity
+@Table(name = "usr")
+public class User implements UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+}
